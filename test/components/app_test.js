7b4c5011ca84552a4c9208c27951b29307e2259b
kiresuah <kiresuah@gmail.com>
2016-05-02 08:44:54 -0400

    initial commit

---
+++ test/components/app_test.js
@@ -0,0 +1,15 @@
+import { renderComponent, expect } from '../test_helper';
+import App from '../../src/components/app';
+
+describe('App', () => {
+
+  beforeEach(() => {
+   
+  });
+
+  it('shows a comment box', () => {
+    
+  });
+
+});
+
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require("webpack");
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    './src/index.js'
+  ],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
