6ad4e44d68b2f6338f6236e656ffe22a962f4aa1
kiresuah <kiresuah@gmail.com>
2016-05-15 19:13:21 -0400

    Updated typings

---
+++ test/actions/components/hello_test.tsx
@@ -0,0 +1,19 @@
+import { assert } = require('chai');
+import { shallow, render } = require('enzyme');
+import * as sinon from 'sinon';
+
+import * as React from 'react';
+import Hello from '../../../src/components/Hello';
+
+describe('Hello', () => {
+
+  beforeEach(() => {
+   
+  });
+
+  it('', () => {
+    
+  });
+
+});
+
