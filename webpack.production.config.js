528fb10068d6f383140e4222f71fe4cbce2d6299
kiresuah <kiresuah@gmail.com>
2016-07-05 19:24:08 -0400

    updated boilerplate

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,37 +1,38 @@
 var path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+require('es6-promise').polyfill()
 
 module.exports = {
   devtool: 'cheap-module-source-map',
   entry: [
-    './src/index'
+    './src/index',
   ],
   output: {
 	/**
 	* output defines where our bundle.min.js and bundle.min.css
     * files will be put. In this configuration we are putting them
-    * in the '/dist' folder in our root directory. In our 
+    * in the '/dist' folder in our root directory. In our
 	* server.production.js we define this '/dist' folder as a static
 	* resource, and our Isomorphic html points to it to retrieve these
 	* files.
 	*/
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.min.js',
-    publicPath: '/static/'
+    publicPath: '/static/',
   },
   plugins: [
     new webpack.DefinePlugin({
-	  /** 
+	  /**
 	  *   tell React we are in production mode, this will eliminate
 	  *   some development tools we don't need in production
 	  */
       'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
+        'NODE_ENV': JSON.stringify('production'),
+      },
     }),
     new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false }
+      compress: { warnings: false },
     }),
 	/**
 	* this will analyze all of the SASS files and bundle them into
@@ -39,24 +40,24 @@ module.exports = {
 	* configuration above - so for this project, '/dist/bundle.min.css'
 	*/
     new ExtractTextPlugin('/bundle.min.css', {
-      allChunks: true
-    })
+      allChunks: true,
+    }),
   ],
   module: {
     loaders: [{
       test: /\.js$/,
       loaders: ['babel'],
-      include: path.join(__dirname, 'src')
+      include: path.join(__dirname, 'src'),
     },
     {
-	  // send all SASS files into the ExtractTextPlugin	
+	  // send all SASS files into the ExtractTextPlugin
       test: /\.scss$/,
-      loader: ExtractTextPlugin.extract('css!sass')
+      loader: ExtractTextPlugin.extract('css!sass'),
     },
     {
       test: /\.jpe?g$|\.gif$|\.png$/i,
-      loader: 'file-loader?name=/img/[name].[ext]'
-    }
-]
-  }
+      loader: 'file-loader?name=/img/[name].[ext]',
+    },
+],
+  },
 };
