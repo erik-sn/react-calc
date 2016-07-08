e2bc2ad62ea11ad8e8f566433293604402e77e5c
kiresuah <kiresuah@gmail.com>
2016-07-08 17:14:53 -0400

    added more tests

--- test/components/numpad_test.js
+++ test/components/numpad_test.js
@@ -1,22 +1,29 @@
 import { renderComponent, expect } from '../test_helper';
-
 import Numpad from '../../src/components/numpad';
 
-describe('Lets write tests!' , () => {
+describe('Numpad' , () => {
+  let component;
+  const click = () => { return 'test working'}
+  const props = {click: click};
 
-  describe('Hello' , () => {
-    let component;
-    const props = {};
+  beforeEach(() => {
+    component = renderComponent(Numpad, props);
+  });
 
-    beforeEach(() => {
-      component = renderComponent(Numpad, props);
-    });
+  it('renders something', () => {
+    expect(component).to.exist;     
+  });
 
-    it('renders something', () => {
-      expect(component).to.exist;
-    });
+  it('has 6 rows', () => {
+    expect(component.find('.button-row').length).to.equal(6);
+  });
 
+  it('has 23 buttons', () => { 
+    expect(component.find('.button').length).to.equal(23);
+  });
 
+  it('does something on click', () => {
+    console.log(component.click())
   });
   
 });
